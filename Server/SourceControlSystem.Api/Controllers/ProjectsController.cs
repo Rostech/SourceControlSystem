24ab59a34a0f98ce1728b32d49d09b828e3956ab
Rostislav <Rostislav@DESKTOP-EI4TEBB>
2016-04-25 20:33:29 +0300

    Services are added.

--- Server/SourceControlSystem.Api/Controllers/ProjectsController.cs
+++ Server/SourceControlSystem.Api/Controllers/ProjectsController.cs
@@ -4,22 +4,26 @@
     using System.Linq;
 
     using SourceControlSystem.Data;
-
+    using SourceControlSystem.Models;
+    using Common.Constants;
     public class ProjectsController : ApiController
     {
-        private readonly SourceControlSystemDbContext db;
+        private readonly IRepository<SoftwareProject> projects;
+        private readonly IRepository<User> users;
 
         public ProjectsController()
         {
-            this.db = new SourceControlSystemDbContext();
+            var db = new SourceControlSystemDbContext();
+            this.users = new EfGenericRepository<User>(db);
         }
 
         public IHttpActionResult Get()
         {
-            var result = this.db
-                .SoftwareProjcets
+            var result = this.projects
+                .All()
                 .OrderByDescending(pr => pr.CreatedOn)
-                .Take(10)
+                .Skip(0)
+                .Take(GlobalConstants.DefaultPageSize)
                 .Select(Models.Projects.SoftwareProjectDetailsResponseModel.FromModel)
                 .ToList();
 
@@ -34,19 +38,15 @@
                 return this.BadRequest("Project name can't be null or empty.");
             }
 
-            var result = this.db
-                .SoftwareProjcets
-                .Where(pr => pr.Name == id)
+            var result = this.projects
+                .All()
+                .Where(pr => pr.Name == id && (!pr.Private && pr.Users.Any(c => c.UserName == this.User.Identity.Name)))
+                .Select(Models.Projects.SoftwareProjectDetailsResponseModel.FromModel)
                 .FirstOrDefault();
 
             if (result == null)
             {
                 return this.NotFound();
-            }
-
-            if (result.Private && !result.Users.Any(c => c.UserName == this.User.Identity.Name))
-            {
-                return this.Unauthorized();
             }
 
             return this.Ok(result);
@@ -55,8 +55,8 @@
         [Authorize]
         public IHttpActionResult Post(Models.Projects.SaveProjectRequestModel model)
         {
-            var currentUser = this.db
-                .Users
+            var currentUser = this.users
+                .All()
                 .FirstOrDefault(u => u.UserName == this.User.Identity.Name);
 
             var newProject = new SourceControlSystem.Models.SoftwareProject
@@ -69,17 +69,17 @@
 
             newProject.Users.Add(currentUser);
 
-            this.db.SoftwareProjcets.Add(newProject);
-            this.db.SaveChanges();
+            this.projects.Add(newProject);
+            this.projects.SaveChanges();
 
             return this.Ok(newProject.Id);
         }
 
         [Route("api/projects/all")]
-        public IHttpActionResult Get(int page, int pageSize = 10)
+        public IHttpActionResult Get(int page, int pageSize = GlobalConstants.DefaultPageSize)
         {
-            var result = this.db
-                   .SoftwareProjcets
+            var result = this.projects
+                   .All()
                    .OrderByDescending(pr => pr.CreatedOn)
                    .Skip((page - 1) * pageSize)
                    .Take(pageSize)
---
+++ Services/SourceControlSystem.Services.Data/Contracts/IProjectsService.cs
@@ -0,0 +1,11 @@
+﻿namespace SourceControlSystem.Services.Data.Contracts
+{
+    using SourceControlSystem.Common.Constants;
+    using SourceControlSystem.Models;
+    using System.Linq;
+
+    public interface IProjectsService
+    {
+        IQueryable<SoftwareProject> All(int page = 1, int pageSize = GlobalConstants.DefaultPageSize);
+    }
+}
